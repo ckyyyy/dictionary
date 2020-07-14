0e6bb278b7c0b3c2d5cb10824e4693b62a5754e0
ckyyyy <chongkyxxx@gmail.com>
2020-07-14 14:48:40 +0800

    refactor into pipelines

--- lib/dictionary.ex
+++ lib/dictionary.ex
@@ -12,13 +12,15 @@ defmodule Dictionary do
       :world
 
   """
-  
+
   def random_word do
-    Enum.random(word_list())
+    word_list()
+    |> Enum.random()
   end
 
   def word_list do
-    content = File.read!("assets/words.txt")
-    list = String.split(content, ~r/\n/)
+    "assets/words.txt"
+    |> File.read!()
+    |> String.split(~r/\n/)
   end
 end
